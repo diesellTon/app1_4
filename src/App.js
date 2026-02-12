2dcae55f6da2359749bbff1f8243cf963e32d3ca
diesellTon <diesell0702@gmail.com>
2026-02-12 10:03:01 +0700

    navlink
    navlink

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React from "react";
-import { userContext } from "./context";
-import Content2 from "./context-content2";
-import Header2 from "./context-header2";
+import Router2 from "./router2";
 
 function App() {
-  let [user,setUser] = React.useState('')
-  return(
-    <userContext.Provider value={[user,setUser]}>
-     <Header2/>
-     <Content2/>
-    </userContext.Provider>
-  )
+  return <Router2/>
 }
 
 export default App;
---
+++ src/products.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './products.css'
+
+export default class Products {
+    data = [
+        ['React',500],
+        ['React Active',650],
+        ['Node.js',450],
+        ['MongoDB',300],
+        ['Express.js',650]
+    ]
+
+     
+}
