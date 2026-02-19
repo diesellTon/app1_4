69d3fbace66aac29cd03fc8ef52266926ee4883d
diesellTon <diesell0702@gmail.com>
2026-02-19 10:33:29 +0700

    Routes
    Routes

--- src/products.js
+++ src/products.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './products.css'
 
-export default class Products {
+export default class Products extends React.Component{
     data = [
         ['React',500],
         ['React Active',650],
@@ -9,6 +9,17 @@ export default class Products {
         ['MongoDB',300],
         ['Express.js',650]
     ]
-
+ render(){
+    return(
+        <table border="1">
+            <tr><th>ชื่อ</th><th>ราคา</th></tr>
+            {
+                this.data.map(d => {
+                    return <tr><td>{d[0]}</td><td>{d[1]}</td></tr>
+                })
+            }
+        </table>
+    )
+ }
      
 }
